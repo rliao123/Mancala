9ab66f9729fd1b57f7544bc047a9c09d1aa56b1c
rliao123 <92598518+rliao123@users.noreply.github.com>
2022-11-02 21:29:32 -0700

    not done

--- src/Mancala.java
+++ src/Mancala.java
@@ -1,137 +1,136 @@
+import java.util.ArrayList;
 
 public class Mancala {
 
-	private int a1;
-	private int a2;
-	private int a3;
-	private int a4;
-	private int a5;
-	private int a6;
-	private int b1;
-	private int b2;
-	private int b3;
-	private int b4;
-	private int b5;
-	private int b6;
+	private ArrayList<Integer> a = new ArrayList<Integer>();
+	private  ArrayList<Integer> b = new ArrayList<Integer>();
+
 	private int mancalaA;
 	private int mancalaB;
 	private int num;
 
 	public Mancala(int num) {
 		this.num = num;
-		a1 = num;
-		a2 = num;
-		a3 = num;
-		a4 = num;
-		a5 = num;
-		a6 = num;
+		
+		for(int i = 0;i<7;i++) {
+			
+			
+			a.add(num);
+		}
 
-		b1 = num;
-		b2 = num;
-		b3 = num;
-		b4 = num;
-		b5 = num;
-		b6 = num;
+		for(int i = 0;i<7;i++) {
+			b.add(num);
+		}
 
 		mancalaA = 0;
 		mancalaB = 0;
 	}
-
-	public int getA1() {
-		return a1;
+	
+	public ArrayList<Integer> getA() {
+		return a;
+	}
+	
+	public ArrayList<Integer> getB() {
+		return b;
 	}
 
-	public void addA1() {
-		a1++;
-	}
-
-	public int getA2() {
-		return a2;
-	}
-
-	public void addA2() {
-		a2++;
-	}
-
-	public int getA3() {
-		return a3;
-	}
-
-	public void addA3() {
-		a3++;
-	}
-
-	public int getA4() {
-		return a4;
-	}
-
-	public void addA4() {
-		a4++;
-	}
-
-	public int getA5() {
-		return a5;
-	}
-
-	public void addA5() {
-		a5++;
-	}
-
-	public int getA6() {
-		return a6;
-	}
-
-	public void addA6() {
-		a6++;
-	}
-
-	public int getB1() {
-		return b1;
-	}
-
-	public void addB1() {
-		b1++;
-	}
-
-	public int getB2() {
-		return b2;
-	}
-
-	public void addB2() {
-		b2++;
-	}
-
-	public int getB3() {
-		return b3;
-	}
-
-	public void addB3() {
-		b3++;
-	}
-
-	public int getB4() {
-		return b4;
-	}
-
-	public void addB4() {
-		b4++;
-	}
-
-	public int getB5() {
-		return b5;
-	}
-
-	public void addB5() {
-		b5++;
-	}
-
-	public int getB6() {
-		return b6;
-	}
-
-	public void addB6() {
-		b6++;
-	}
+//	public int getA1() {
+//		return a1;
+//	}
+//	
+//
+//
+//	public void addA1() {
+//		a1++;
+//	}
+//
+//	public int getA2() {
+//		return a2;
+//	}
+//
+//	public void addA2() {
+//		a2++;
+//	}
+//
+//	public int getA3() {
+//		return a3;
+//	}
+//
+//	public void addA3() {
+//		a3++;
+//	}
+//
+//	public int getA4() {
+//		return a4;
+//	}
+//
+//	public void addA4() {
+//		a4++;
+//	}
+//
+//	public int getA5() {
+//		return a5;
+//	}
+//
+//	public void addA5() {
+//		a5++;
+//	}
+//
+//	public int getA6() {
+//		return a6;
+//	}
+//
+//	public void addA6() {
+//		a6++;
+//	}
+//
+//	public int getB1() {
+//		return b1;
+//	}
+//
+//	public void addB1() {
+//		b1++;
+//	}
+//
+//	public int getB2() {
+//		return b2;
+//	}
+//
+//	public void addB2() {
+//		b2++;
+//	}
+//
+//	public int getB3() {
+//		return b3;
+//	}
+//
+//	public void addB3() {
+//		b3++;
+//	}
+//
+//	public int getB4() {
+//		return b4;
+//	}
+//
+//	public void addB4() {
+//		b4++;
+//	}
+//
+//	public int getB5() {
+//		return b5;
+//	}
+//
+//	public void addB5() {
+//		b5++;
+//	}
+//
+//	public int getB6() {
+//		return b6;
+//	}
+//
+//	public void addB6() {
+//		b6++;
+//	}
 
 	public int getMancalaA() {
 		return mancalaA;
@@ -152,4 +151,28 @@ public class Mancala {
 	public int getNum() {
 		return num;
 	}
+//
+//	public void setA1(int a1) {
+//		this.a1 = a1;
+//	}
+//	
+//	public void setA2(int a2) {
+//		this.a2 = a2;
+//	}
+//	
+//	public void setA3(int a3) {
+//		this.a3 = a3;
+//	}
+//	
+//	public void setA4(int a4) {
+//		this.a4 = a4;
+//	}
+//	
+//	public void setA5(int a5) {
+//		this.a5 = a5;
+//	}
+//	
+//	public void setA6(int a6) {
+//		this.a6 = a6;
+//	}
 }
--- src/MancalaTester.java
+++ src/MancalaTester.java
@@ -3,6 +3,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import javax.swing.JButton;
@@ -19,6 +20,8 @@ public class MancalaTester {
 		String numOfStones = s.nextLine();
 		int num = Integer.parseInt(numOfStones);
 		Mancala m = new Mancala(num);
+		ArrayList<Integer> a = m.getA();
+		ArrayList<Integer> b = m.getB();
 
 		JFrame frame = new JFrame("Mancala");
 		frame.setSize(400, 400);
@@ -39,83 +42,128 @@ public class MancalaTester {
 		JPanel south = new JPanel();
 		south.add(playerA);
 		frame.add(south, BorderLayout.SOUTH); // player A
+		
+		JButton mB = new JButton(String.valueOf(m.getMancalaA())); // mancala B button
+		JButton mA = new JButton(String.valueOf(m.getMancalaB())); // mancala A button
+		mB.setPreferredSize(new Dimension(50, 100));
+		mA.setPreferredSize(new Dimension(50, 100));
 
-		JLabel b = new JLabel("      B6          B5          B4          B3          B2         B1");
-		JButton b6 = new JButton(String.valueOf(m.getB6()));
+		JLabel bLabel = new JLabel("      B6          B5          B4          B3          B2         B1");
+
+		JButton[] bButtons = new JButton[6];
+
+		JButton b6 = new JButton(String.valueOf(b.get(5)));
 		b6.setPreferredSize(new Dimension(50, 50));
-		b6.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-
-				if (m.getNum() == 3) {
-					m.addMancalB();
-					m.addA1();
-					m.addA2();
-
-				} else if (m.getNum() == 4) {
-					m.addMancalB();
-					m.addA1();
-					m.addA2();
-					m.addA3();
-				}
-
-				b6.setText("0");
-
-
-			}
-
-		});
-		JButton b5 = new JButton(String.valueOf(m.getB5()));
+		JButton b5 = new JButton(String.valueOf(b.get(4)));
 		b5.setPreferredSize(new Dimension(50, 50));
-		JButton b4 = new JButton(String.valueOf(m.getB4()));
+		JButton b4 = new JButton(String.valueOf(b.get(3)));
 		b4.setPreferredSize(new Dimension(50, 50));
-		JButton b3 = new JButton(String.valueOf(m.getB3()));
+		JButton b3 = new JButton(String.valueOf(b.get(2)));
 		b3.setPreferredSize(new Dimension(50, 50));
-		JButton b2 = new JButton(String.valueOf(m.getB2()));
+		JButton b2 = new JButton(String.valueOf(b.get(1)));
 		b2.setPreferredSize(new Dimension(50, 50));
-		JButton b1 = new JButton(String.valueOf(m.getB1()));
+		JButton b1 = new JButton(String.valueOf(b.get(0)));
 		b1.setPreferredSize(new Dimension(50, 50));
+
+		bButtons[0] = b1;
+		bButtons[1] = b2;
+		bButtons[2] = b3;
+		bButtons[3] = b4;
+		bButtons[4] = b5;
+		bButtons[5] = b6;
 
 		JPanel centerTop1 = new JPanel();
 		centerTop1.setLayout(new BorderLayout());
-		centerTop1.add(b, BorderLayout.NORTH);
+		centerTop1.add(bLabel, BorderLayout.NORTH);
 
 		JPanel centerTop2 = new JPanel();
-		centerTop2.add(b6);
-		centerTop2.add(b5);
-		centerTop2.add(b4);
-		centerTop2.add(b3);
-		centerTop2.add(b2);
-		centerTop2.add(b1);
+		for (int i = 0; i < bButtons.length; i++) {
+			centerTop2.add(bButtons[i]);
+		}
 
 		centerTop1.add(centerTop2, BorderLayout.SOUTH);
 
-		JLabel a = new JLabel("      A1         A2          A3         A4          A5         A6");
-		JButton a6 = new JButton(String.valueOf(m.getA6()));
+		JLabel aLabel = new JLabel("      A1         A2          A3         A4          A5         A6");
+
+		JButton[] aButtons = new JButton[6];
+		JButton a6 = new JButton(String.valueOf(a.get(5)));
 		a6.setPreferredSize(new Dimension(50, 50));
-		JButton a5 = new JButton(String.valueOf(m.getA5()));
+		JButton a5 = new JButton(String.valueOf(a.get(4)));
 		a5.setPreferredSize(new Dimension(50, 50));
-		JButton a4 = new JButton(String.valueOf(m.getA4()));
+		JButton a4 = new JButton(String.valueOf(a.get(3)));
 		a4.setPreferredSize(new Dimension(50, 50));
-		JButton a3 = new JButton(String.valueOf(m.getA3()));
+		JButton a3 = new JButton(String.valueOf(a.get(2)));
 		a3.setPreferredSize(new Dimension(50, 50));
-		JButton a2 = new JButton(String.valueOf(m.getA2()));
+		JButton a2 = new JButton(String.valueOf(a.get(1)));
 		a2.setPreferredSize(new Dimension(50, 50));
-		JButton a1 = new JButton(String.valueOf(m.getA1()));
+		JButton a1 = new JButton(String.valueOf(a.get(0)));
 		a1.setPreferredSize(new Dimension(50, 50));
+
+		aButtons[0] = a1;
+		aButtons[1] = a2;
+		aButtons[2] = a3;
+		aButtons[3] = a4;
+		aButtons[4] = a5;
+		aButtons[5] = a6;
+
+		aButtons[0].addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (m.getA().get(0) != 0) {
+
+					for (int i = 0; i <= a.get(0); i++) {
+						int temp = a.get(i) + 1;
+						a.set(i, temp);
+						System.out.println(a.get(i));
+					}
+					a.set(0, 0);
+					aButtons[0].setText("0");
+					aButtons[1].setText(String.valueOf(a.get(1)));
+					aButtons[2].setText(String.valueOf(a.get(2)));
+					aButtons[3].setText(String.valueOf(a.get(3)));
+
+					if (m.getNum() == 4) {
+						aButtons[4].setText(String.valueOf(a.get(4)));
+					}
+
+				}
+			}
+		});
+		
+		aButtons[1].addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (m.getA().get(1) != 0) {
+
+					for (int i = 1; i <= a.get(1); i++) {
+						int temp = a.get(i) + 1;
+						a.set(i, temp);
+						System.out.println(a.get(i));
+					}
+					a.set(1, 0);
+					aButtons[1].setText("0");
+					aButtons[2].setText(String.valueOf(a.get(2)));
+					aButtons[3].setText(String.valueOf(a.get(3)));
+					aButtons[4].setText(String.valueOf(a.get(4)));
+
+					if (m.getNum() == 4) {
+						aButtons[5].setText(String.valueOf(a.get(5)));
+					}
+
+				}
+			}
+		});
 
 		JPanel centerBottom1 = new JPanel();
 		centerBottom1.setLayout(new BorderLayout());
-		centerBottom1.add(a, BorderLayout.SOUTH);
+		centerBottom1.add(aLabel, BorderLayout.SOUTH);
 
 		JPanel centerBottom2 = new JPanel();
-		centerBottom2.add(a6);
-		centerBottom2.add(a5);
-		centerBottom2.add(a4);
-		centerBottom2.add(a3);
-		centerBottom2.add(a2);
-		centerBottom2.add(a1);
+		for (int i = 0; i < aButtons.length; i++) {
+			centerBottom2.add(aButtons[i]);
+		}
 
 		centerBottom1.add(centerBottom2, BorderLayout.NORTH);
 
@@ -124,10 +172,7 @@ public class MancalaTester {
 		center.add(centerTop1, BorderLayout.NORTH); // B6,B5,B4,B3,B3,B1 and corresponding square button
 		center.add(centerBottom1, BorderLayout.SOUTH); // A1,A2,A3,A4,A5,A6 and corresponding square button
 
-		JButton mB = new JButton(String.valueOf(m.getMancalaA())); // mancala B button
-		JButton mA = new JButton(String.valueOf(m.getMancalaB())); // mancala A button
-		mB.setPreferredSize(new Dimension(50, 100));
-		mA.setPreferredSize(new Dimension(50, 100));
+		
 
 		JPanel board = new JPanel(); // mancala board
 		board.setLayout(new BorderLayout());
